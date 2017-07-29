8fbbff1c947414b088d8b3d788c9ae7ef8bbf81a
Polishchuk Oleh <Polishchuk Oleh>
2017-07-29 08:38:11 +0300

    Created scripts for deploy of docker-compose services: "api" and "web"

---
+++ scripts/rr-web-app.sh
@@ -0,0 +1,32 @@
+ROOT="/usr/src/app"
+NAME="rr-web-app"
+DOCKER="$ROOT/rr-docker"
+DOCKER_SERVICE="web"
+APPLICATION="$ROOT/$NAME"
+BACKUP="$ROOT/$NAME-backup"
+
+
+cd "$ROOT/"
+ls
+
+if [ -d "$BACKUP" ] && [ -d "$APPLICATION" ]; then
+    echo "Remove previously created backup..."
+    rm -rf "$BACKUP"
+fi
+echo "No one backups found!"
+
+if [ -d "$APPLICATION" ]; then
+    echo "Create new one backup..."
+    mv "$APPLICATION" "$BACKUP"
+fi
+
+mv "$APPLICATION" "$BACKUP"
+echo "Created new one backup!"
+
+git clone -b develop "https://github.com/royalrangers-ck/$NAME.git"
+
+cd "$DOCKER"
+docker-compose ps
+docker-compose stop "$DOCKER_SERVICE"
+docker-compose rm -f -s -v "$DOCKER_SERVICE"
+docker-compose up -d --build "$DOCKER_SERVICE"
